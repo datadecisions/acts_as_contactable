7c059e81d443793efa926a798e7b388ac02ade60
Tyler Rick <github.com@tylerrick.com>
2011-08-03 13:32:58 -0700

    address was misspelled as addrress

--- generators/contactable/templates/address_migration.rb
+++ generators/contactable/templates/address_migration.rb
@@ -2,8 +2,8 @@ class <%= migration_name %> < ActiveRecord::Migration
   def self.up
     create_table "<%= table_name %>", :force => true do |t|
       t.column :name,                     :string
-      t.column :addrress_1,               :string
-      t.column :addrress_2,               :string
+      t.column :address_1,               :string
+      t.column :address_2,               :string
       t.column :city,                     :string
       t.column :state,                    :string
       t.column :zip,                      :string
